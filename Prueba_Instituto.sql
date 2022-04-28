a702f2d8bdc3214e3b0ce028801cb2fc87fc086b
Francisco Gracia Rodriguez <101580554+DEADG616D@users.noreply.github.com>
2022-04-28 15:33:30 +0200

    Add files via upload
    Esta es la base de datos que Estaría destinada a  los institutos, por si alguien la quiere probar también he puesto un pequeño archivo que sirve como prueba para la base de datos de los institutos.

---
+++ Prueba_Instituto.sql
@@ -0,0 +1,12 @@
+#Pruebas
+INSERT INTO Usuarios (usu_correo, usu_nombre, usu_apellido, usu_tipo, usu_clase) VALUES
+('Prueba@gmail.com', 'Francisco', 'Garcia', 'Administrador', 'SMX2B'),
+('Prueba2@gmail.com', 'Jesus', 'Gomez', 'Administrador', 'SMX2B');
+
+INSERT INTO Producto (pro_nombre, pro_descripcion, pro_familia, pro_estado, pro_fecha_compra, pro_precio, pro_solicitante_compra) VALUES
+('Raspberry pi 4 B', 'Sin cargador', 'RASPBERRY', 'EN PERFECTO ESTADO', '2022-01-05', '101.99', 'Fernando Rivero'),
+('Raspberry pi 3 A', 'Con gamepad', 'RASPBERRY', 'EN PERFECTO ESTADO', '2022-01-04', '111.99', 'Fernando Rivero');
+
+INSERT INTO Prestamo (usu_correo, pre_fecha_prestamo, pro_id, pre_fecha_devolucion, pre_notas) VALUES
+('Prueba@gmail.com', '2022-03-29', '1', '2022-03-30', 'Devuelto con todos los accesorios'),
+('Prueba2@gmail.com', '2022-03-30', '2', '2022-03-31', 'Se ha devuelto con un arañazo en la parte superior de 3cm');

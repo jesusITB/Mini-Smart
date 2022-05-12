144ac9956817cb98050f7e9875096c857882a554
Francisco Gracia Rodriguez <101580554+DEADG616D@users.noreply.github.com>
2022-05-12 16:01:17 +0200

    Update Layout_Empresas.sql

--- Layout_Empresas.sql
+++ Layout_Empresas.sql
@@ -10,32 +10,32 @@ CREATE TABLE IF NOT EXISTS Usuarios( #Tabla la cual almacena a los usuarios.
     usu_id BIGINT NOT NULL, #Id de la tarjeta nfc a la cual se va a vincular esta persona.
     usu_nombre VARCHAR(45) NOT NULL, #Nombre de la persona.
     usu_apellido VARCHAR(45) NOT NULL, #Apellido de la persona.
-    usu_tipo ENUM('Alumno', 'Profe', 'Administrador') NOT NULL, #Tipo de usuario y permisos que tendra la persona.
-    usu_departmento ENUM('Ventas','Produccion','Marketing','Desarrollo','I+D','Montaje','Administrativo') NOT NULL, #departamento al cual pertenece la persona, se puede añadir cualquiera en base a las necesidades de la empresa.
+    usu_tipo ENUM('Alumno', 'Profe', 'Administrador') NOT NULL, #Tipo de usuario y permisos que tendrá la persona.
+ usu_departmento ENUM('Ventas','Produccion','Marketing','Desarrollo','I+D','Montaje','Administrativo') NOT NULL, #departamento al cual pertenece la persona.
     PRIMARY KEY (usu_correo) 
 );
 
 CREATE TABLE IF NOT EXISTS Producto( #Tabla en la cual se guardan los productos del almacén.
-	pro_id INT NOT NULL AUTO_INCREMENT, #Id del producto para poder utilizarlo en la tabla prestamo.
+	pro_id INT NOT NULL AUTO_INCREMENT, #Id del producto para poder utilizarlo en la tabla préstamo.
     pro_nombre VARCHAR(45) NOT NULL, #Nombre del producto, ej: Raspberry pi 4, Oculus quest 2.
-    pro_descripcion VARCHAR(45) NOT NULL, #Aqui se especifica si trae cargador, gamepad, etc...
+    pro_descripcion VARCHAR(45) NOT NULL, #Aquí se especifica si trae cargador, gamepad, etc...
     pro_familia VARCHAR(45) NOT NULL, #Tipo de producto, ej; Raspberry, Gafas VR, Arduino.
     pro_estado ENUM('EN PERFECTO ESTADO', 'EN BUEN ESTADO', 'EN ESTADO NORMAL', 'EN MAL ESTADO') NOT NULL, #Estado del producto.
-    pro_fecha_compra DATE NOT NULL, #Fecha en la que se compro el producto, sirve para poder tener una guia de como de util a sido el producto.
+    pro_fecha_compra DATE NOT NULL, #Fecha en la que se compró el producto, sirve para poder tener una guía de cómo de útil ha sido el producto.
     pro_precio FLOAT NOT NULL, #Precio de compra del producto.
     pro_solicitante_compra VARCHAR(45) NOT NULL, #Persona que ha solicitado la compra del producto.
     PRIMARY KEY (pro_id)
 );
 
 #Tabla master
-CREATE TABLE IF NOT EXISTS Prestamo( #Tabla para guardar los presamos realizados.
-	pre_id INT NOT NULL AUTO_INCREMENT, #Id de la tabla producto para poder tener los prestamos organizados.
-    usu_correo VARCHAR(320) NOT NULL, #Correo electrónico de la persona, el cual esta vinculado con la tabla de Usuarios.
-    pro_id INT NOT NULL, #Id del producto, este campo esta vinculado con la tabla Prestamo.
-    pre_fecha_prestamo DATE NOT NULL, #Fecha del dia del prestamo.
-    pre_fecha_devolucion DATE, #fecha del dia que se devuelve el producto
-    pre_notas VARCHAR(420) NULL, #Notas sobre el prestamo, ej: se ha devuelto roto.
+CREATE TABLE IF NOT EXISTS Prestamo( #Tabla para guardar los préstamos realizados.
+	pre_id INT NOT NULL AUTO_INCREMENT, #Id de la tabla producto para poder tener los préstamos organizados.
+    usu_correo VARCHAR(320) NOT NULL, #Correo electrónico de la persona, el cual está vinculado con la tabla de Usuarios.
+    pro_id INT NOT NULL, #Id del producto, este campo está vinculado con la tabla Préstamo.
+    pre_fecha_prestamo DATE NOT NULL, #Fecha del dia del préstamo.
+    pre_fecha_devolucion DATE, #fecha del día que se devuelve el producto.
+    pre_notas VARCHAR(420) NULL, #Notas sobre el préstamo, ej: se ha devuelto roto.
     PRIMARY KEY (pre_id), 
-    FOREIGN KEY (usu_correo) REFERENCES Usuarios(usu_correo) ON UPDATE CASCADE, #vinculacion de la tabla Usuarios con esta, ademas cuenta con el ON UPDATE CASCADE que sirve para que cuando cambiemos algo en las otras tablas se cambie en esta.
-    FOREIGN KEY (pro_id) REFERENCES Producto(pro_id) ON UPDATE CASCADE  #vinculacion de la tabla Producto con esta, ademas cuenta con el ON UPDATE CASCADE que sirve para que cuando cambiemos algo en las otras tablas se cambie en esta.
+    FOREIGN KEY (usu_correo) REFERENCES Usuarios(usu_correo) ON UPDATE CASCADE, #vinculación de la tabla Usuarios con esta, además cuenta con el ON UPDATE CASCADE que sirve para que cuando cambiemos algo en las otras tablas se cambie en esta.
+    FOREIGN KEY (pro_id) REFERENCES Producto(pro_id) ON UPDATE CASCADE  #vinculación de la tabla Producto con esta, además cuenta con el ON UPDATE CASCADE que sirve para que cuando cambiemos algo en las otras tablas se cambie en esta.
 );
